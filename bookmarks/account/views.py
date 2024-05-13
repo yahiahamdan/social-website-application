14abca93fd712024018681cbafb9ef68412e5db8
yahia hamdan <hamdanyahia2@gmail.com>
2024-05-13 20:08:41 +0300

    adding login required decorartor in view to loginfirst

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -2,8 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate,login
 from .forms import loginForm
+from django.contrib.auth.decorators import login_required
+"""
+login-required decorator of the authentication framework 
+check lw current user dh authenticated it executes the decorater view 
+it redirects to login url with the  orignalyy request url as a get param
 
-# Create your views here. 
+"""
+
+
+# Create your views here.
+@login_required 
+def dashboard(request):
+    return render(request,'account/dashboard.html',{'section':'dashboard'})
+
 def user_login(request):
     if request.method=='POST':
         form=loginForm(request.POST)
