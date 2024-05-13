f8141c49537075886a170abc6567e21492135eb3
yahia hamdan <hamdanyahia2@gmail.com>
2024-05-13 19:42:18 +0300

    updating the url

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -5,6 +5,6 @@ from .import views
 app_name='account'
 urlpatterns = [
     #path('login/',views.user_login,name='login'),
-    path('login/',auth_views.LoginView.as_view,name='login')
-    ,path('logout/',auth_views.LogoutView.as_view,name='logout')
+    path('login/',auth_views.LoginView.as_view(),name='login')
+    ,path('logout/',auth_views.LogoutView.as_view(),name='logout')
 ]
