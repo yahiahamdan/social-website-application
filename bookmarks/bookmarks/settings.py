eb8501fedc40de262783f551a84e62f33904e8ce
yahia hamdan <hamdanyahia2@gmail.com>
2024-05-13 19:03:47 +0300

    update the settings

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'account.apps.AccountConfig',
 ]
 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -117,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
