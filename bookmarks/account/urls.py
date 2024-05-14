5dad60dd0bd1daccb8788efd3a9bcd1a8378fb09
yahia hamdan <hamdanyahia2@gmail.com>
2024-05-14 19:24:23 +0300

    using reset url build in functions

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -11,4 +11,13 @@ urlpatterns = [
 #password urls
 ,path('password-change/',auth_views.PasswordChangeView.as_view(),name='password_change')
 ,path('password-change/done',auth_views.PasswordChangeDoneView.as_view(),name='password_change_done')
+#resst password urls
+,
+path('password-reset/', auth_views.PasswordResetView.as_view(
+    email_template_name='registration/password_reset_email.html',
+     subject_template_name='registration/password_reset_subject.txt'
+), name='password_reset'),
+path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+path('password-reset/confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
