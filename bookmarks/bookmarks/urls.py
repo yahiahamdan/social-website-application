bbbfbc59959785e0822e326fa118e374abcd9b46
yahia hamdan <hamdanyahia2@gmail.com>
2024-05-14 19:46:12 +0300

    adding urls.py in the to inclue auth

--- bookmarks/bookmarks/urls.py
+++ bookmarks/bookmarks/urls.py
@@ -20,4 +20,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/',include('account.urls',namespace='account')),
-]
+    path('account/',include('django.contrib.auth.urls')),
+
+
+]   
