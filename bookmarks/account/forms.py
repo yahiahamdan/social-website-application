e9cddea938b630cd1e395f57df1f19f33db26ed1
yahia hamdan <hamdanyahia2@gmail.com>
2024-05-18 16:10:40 +0300

    user registeration form

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import User
 """
 we zey ma olna deh wedgiet 
 widget bt5lehaa lma t3mlha render   browser treat as passwo input
@@ -6,3 +7,15 @@ widget bt5lehaa lma t3mlha render   browser treat as passwo input
 class loginForm(forms.Form):
     username=forms.CharField()
     password=forms.CharField(widget=forms.PasswordInput)
+
+class UserRegistrationForm(forms.ModelForm):
+    password=forms.CharField(label='Password',widget=forms.PasswordInput)
+    password2=forms.CharField(label='Repeat Password',widget=forms.PasswordInput)
+    class Meta:
+        model=User
+        fields=('username','first_name','email')
+    def clean_password2(self):
+        cd=self.cleaned_data
+        if cd['password']!=cd['password2']:
+            raise forms.ValidationError('passwords dont match')
+        return cd['password2']
