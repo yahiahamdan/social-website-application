a08377cc6885e5eef711a6baf1a676789debb9bf
yahia hamdan <hamdanyahia2@gmail.com>
2024-05-18 21:12:11 +0300

    adding edit url

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -5,6 +5,7 @@ from .import views
 app_name='account'
 urlpatterns = [
     #path('login/',views.user_login,name='login'),
+    path('edit/',views.edit,name='edit'),
     path('register/',views.register,name='register'),
     path('login/',auth_views.LoginView.as_view(),name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
