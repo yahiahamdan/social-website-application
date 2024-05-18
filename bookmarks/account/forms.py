2f74f58505766a51a5774ccdaf17c63b8cb52d3f
yahia hamdan <hamdanyahia2@gmail.com>
2024-05-18 21:14:05 +0300

    adding userEditForm and ProfileEditForm for editing

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import Profile
 """
 we zey ma olna deh wedgiet 
 widget bt5lehaa lma t3mlha render   browser treat as passwo input
@@ -19,3 +20,12 @@ class UserRegistrationForm(forms.ModelForm):
         if cd['password']!=cd['password2']:
             raise forms.ValidationError('passwords dont match')
         return cd['password2']
+    
+class UserEditForm(forms.ModelForm):
+     class Meta:
+         model=User
+         fields=['first_name','last_name','email']
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model=Profile
+        fields=['date_of_birth','photo']
