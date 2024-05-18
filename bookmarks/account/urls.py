45f928259fc6572440b54e1acff41fda61cfa0be
yahia hamdan <hamdanyahia2@gmail.com>
2024-05-18 16:09:32 +0300

    adding url to register and the view to registeration

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
+from django.urls import path,include
 from django.contrib.auth import views as auth_views
 from .import views
 
 app_name='account'
 urlpatterns = [
     #path('login/',views.user_login,name='login'),
+    path('register/',views.register,name='register'),
     path('login/',auth_views.LoginView.as_view(),name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('',views.dashboard,name='dashboard')
@@ -20,4 +21,5 @@ path('password-reset/', auth_views.PasswordResetView.as_view(
 path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
 path('password-reset/confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+path('', include('django.contrib.auth.urls')),
 ]
--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth import authenticate,login
 from .forms import loginForm
 from django.contrib.auth.decorators import login_required
+from .forms import UserRegistrationForm
 """
 login-required decorator of the authentication framework 
 check lw current user dh authenticated it executes the decorater view 
@@ -37,3 +38,14 @@ def user_login(request):
      form=loginForm()
     return render(request,'account/login.html',{'form':form})
 
+def register(request):
+    if request.method== 'POST':
+        user_form=UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user=user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return render(request,'account/register_done.html',{'new_user':new_user})
+    else:
+        user_form=UserRegistrationForm()
+    return render(request,'account/register.html',{'user_form':user_form})
