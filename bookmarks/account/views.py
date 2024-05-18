de8f2c0ce43740e7525bdb2c33542037431c0926
yahia hamdan <hamdanyahia2@gmail.com>
2024-05-18 21:15:31 +0300

    adding edit method login required  contianing profileditform and usereditform

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 from django.contrib.auth import authenticate,login
 from .forms import loginForm
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm,UserEditForm,ProfileEditForm
+from .models import Profile
 """
 login-required decorator of the authentication framework 
 check lw current user dh authenticated it executes the decorater view 
@@ -44,8 +45,32 @@ def register(request):
         if user_form.is_valid():
             new_user=user_form.save(commit=False)
             new_user.set_password(user_form.cleaned_data['password'])
+            
             new_user.save()
+            #create the user profile
+            Profile.objects.create(user=new_user)
             return render(request,'account/register_done.html',{'new_user':new_user})
     else:
         user_form=UserRegistrationForm()
     return render(request,'account/register.html',{'user_form':user_form})
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+       user_form = UserEditForm(instance=request.user,
+                             data=request.POST)
+       profile_form=ProfileEditForm(
+           instance=request.user.profile,
+           data=request.Post,
+           files=request.FILES
+                        )
+       if user_form.is_valid() and profile_form.is_valid():
+           user_form.save()
+           profile_form.save()
+    else:
+           user_form=UserEditForm(instance=request.user)
+           profile_form=ProfileEditForm(instance=request.user.profile)
+    return render(request,'account/edit.html',{'user_form':user_form,'profile_form':profile_form})
+
+
+     
