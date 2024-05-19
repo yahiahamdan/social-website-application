8909b436e85a1b10f28d85dbb987adefbcc71698
yahia hamdan <hamdanyahia2@gmail.com>
2024-05-19 18:32:38 +0300

    adding clean email method to prevenet from editing your email

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -30,6 +30,13 @@ class UserEditForm(forms.ModelForm):
      class Meta:
          model=User
          fields=['first_name','last_name','email']
+     def clean_email(self):
+        data = self.cleaned_data["email"]
+        qs=User.objects.filter(email=data).exclude(id=self.instance.id)
+        if qs.exists():
+            raise forms.ValidationError("Email is already in use")
+        return data
+    
 class ProfileEditForm(forms.ModelForm):
     class Meta:
         model=Profile
